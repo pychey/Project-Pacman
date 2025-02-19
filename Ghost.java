73b0ce2bf9717c526c1df246399e82e4606dcfba
vid <seangdavid099@gmail.com>
2025-02-19 23:09:34 +0700

    add ghost and pacman class

---
+++ Ghost.java
@@ -0,0 +1,9 @@
+public class Ghost extends Character{
+
+    public Ghost(int x, int y, String name, char symbol){
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.symbol = symbol;
+    }
+}
