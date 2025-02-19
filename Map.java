a7bcb8e817a111cdfa97d8b482f9f9d9be5062cd
Pychey <prakpycheyy@gmail.com>
2025-02-19 23:37:11 +0700

    Add Generate Food Function To Map

--- Map.java
+++ Map.java
@@ -1,5 +1,6 @@
 public class Map {
     public int width,height;
+    public int foodCount = 0;
     public char[][] grid = {
         {'x','x','x','x','x','x','x','x','x','x','x'},
         {'x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'},
@@ -12,11 +13,23 @@ public class Map {
         {'x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'},
         {'x','x','x','x','x','x','x','x','x','x','x'}
     };
+
     Map() {
         height = grid.length;
         width = grid[0].length;
     }
     public boolean isWithinBound(int x, int y){
         return (x >= 0 && x < width) && (y >= 0 && y < height);
+    }
+
+    public void generateFood(){
+        for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+                if(grid[i][j] == ' '){
+                    grid[i][j] = '.';
+                    foodCount++;
+                }
+            }
+        }
     }
 }
