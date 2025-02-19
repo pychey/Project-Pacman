e5ac79655e786c0d665c7649fc95af140ff18029
HunDavin <davin.hun@student.cadt.edu.kh>
2025-02-20 00:46:32 +0700

    print map pacman and ghost

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+
+
 public class Game {
 
     public Game() {
@@ -8,9 +10,18 @@ public class Game {
 
     //Create map object
     Map map= new Map();
+    //create pacman and ghost
+    Pacman pacman = new Pacman(1,1);
+    Ghost ghosts = new Ghost(8,8,"orange",'o');
+
+
+
 
 
     public void startGame(){
+        map.placePacman(pacman.x, pacman.y);
+        map.placeGhost(ghosts.x, ghosts.y);
+        map.generateFood();
         map.printMap();
     }
 
